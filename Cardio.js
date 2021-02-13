e284f7a38d0693b9a43393d866672102560538d7
timzhen <tim.zhen38@myhunter.cuny.edu>
2021-02-13 14:24:35 -0500

    fixed timestamps for cardio

--- Cardio.js
+++ Cardio.js
@@ -7,7 +7,7 @@ class Cardio {
              new Exercise("Burpees", this.url, "125"),
              new Exercise("Bicycles", this.url, "175"),
              new Exercise("In & Outs, Open & Close", this.url, "217"),
-             new Exercise("Side to Side Jump Squats", this.url, "240"),
+             new Exercise("Side to Side Jump Squats", this.url, "256"),
              new Exercise("Shoulder to Shoulder", this.url, "293"),
              new Exercise("Switching Lunges", this.url, "341"),
              new Exercise("High Knee Runs", this.url, "387")
