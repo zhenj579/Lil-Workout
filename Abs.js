2f086930e15136464cd0af226a2c9565103ccbda
RN0529 <66885775+RN0529@users.noreply.github.com>
2021-02-13 02:32:39 -0500

    bug changes

--- Abs.js
+++ Abs.js
@@ -3,15 +3,15 @@ class Abs {
 	 constructor(url, exercises) {
 	 	this.url = "https://www.youtube.com/watch?v=ofTiKY3hYdE";
 	 	this.exercises = [
-	 		new Exercise("High Knee Taps", this.url, "1m30s"),
-	 		new Exercise("Russian Twists", this.url, "2m22s"),
-	 		new Exercise("Leg Raises", this.url, "3m19s"),
-	 		new Exercise("Hip Raises", this.url, "4m20s"),
-	 		new Exercise("Flutter Kicks", this.url, "5m23s"),
-	 		new Exercise("Plank Knees to Elbow", this.url, "6m18s"),
-	 		new Exercise("Chair Sit Ups", this.url, "7m22s"),
-	 		new Exercise("Seated In and Outs", this.url, "8m17s"),
-	 		new Exercise("Jumping Jacks", this.url, "9m20s")
+	 		new Exercise("High Knee Taps", this.url, "90"),
+	 		new Exercise("Russian Twists", this.url, "82"),
+	 		new Exercise("Leg Raises", this.url, "109"),
+	 		new Exercise("Hip Raises", this.url, "260"),
+	 		new Exercise("Flutter Kicks", this.url, "323"),
+	 		new Exercise("Plank Knees to Elbow", this.url, "378"),
+	 		new Exercise("Chair Sit Ups", this.url, "442"),
+	 		new Exercise("Seated In and Outs", this.url, "497"),
+	 		new Exercise("Jumping Jacks", this.url, "560")
 	 	];
 	 }
    
